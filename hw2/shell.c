6e9270bab68d41523e4a037f3be46670969675cf
potter <472434557@qq.com>
2019-12-17 21:37:11 +0800

    Finished add basic functinality into the shell.

--- hw2/shell.c
+++ hw2/shell.c
@@ -10,6 +10,7 @@
 #include <sys/wait.h>
 #include <termios.h>
 #include <unistd.h>
+#include <limits.h>
 
 #include "tokenizer.h"
 
@@ -30,6 +31,8 @@ pid_t shell_pgid;
 
 int cmd_exit(struct tokens *tokens);
 int cmd_help(struct tokens *tokens);
+int cmd_pwd(struct tokens *tokens);
+int cmd_cd(struct tokens *tokens);
 
 /* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(struct tokens *tokens);
@@ -44,6 +47,8 @@ typedef struct fun_desc {
 fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
   {cmd_exit, "exit", "exit the command shell"},
+  {cmd_pwd, "pwd", "current working directory."},
+  {cmd_cd, "cd", "change the current working directory."},
 };
 
 /* Prints a helpful description for the given command */
@@ -56,6 +61,29 @@ int cmd_help(unused struct tokens *tokens) {
 /* Exits this shell */
 int cmd_exit(unused struct tokens *tokens) {
   exit(0);
+}
+
+int cmd_pwd(struct tokens *tokens)
+{
+	char abs_path_buff[PATH_MAX];
+	getcwd(abs_path_buff, PATH_MAX-1);
+	printf("%s\n", abs_path_buff);
+	return 1;
+}
+
+int cmd_cd(struct tokens *tokens)
+{
+	int nResult = chdir(tokens_get_token(tokens, 1));
+	if (nResult == 0)
+	{
+		cmd_pwd(tokens);
+		return 1;
+	}
+	else
+	{
+		printf("Error Directory.\n");
+		return 0;
+	}	
 }
 
 /* Looks up the built-in command, if it exists. */
